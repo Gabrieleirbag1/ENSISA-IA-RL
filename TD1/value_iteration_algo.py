fd9511bbe812c0b15bfca4928c76a05b797110f8
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-11-16 17:50:53 +0100

    refractor:  value iteration algorithm to streamline policy extraction and improve readability

--- TD1/value_iteration_algo.py
+++ TD1/value_iteration_algo.py
@@ -3,6 +3,7 @@ from utils import policy_int_to_char, policy_one_step_look_ahead
 
 def value_iteration(n, Gamma, threshold):
     v = np.zeros((n, n))
+    pi = np.zeros((n, n), dtype=int)
     
     while True:
         delta = 0
@@ -15,6 +16,8 @@ def value_iteration(n, Gamma, threshold):
                 old_v = v[i, j]
                 
                 max_value = float('-inf')
+                best_max_value = float('-inf')
+                best_action = 0
                 for action in range(4):
                     next_i = i + policy_one_step_look_ahead[action][0]
                     next_j = j + policy_one_step_look_ahead[action][1]
@@ -22,39 +25,20 @@ def value_iteration(n, Gamma, threshold):
                     if next_i < 0 or next_i >= n or next_j < 0 or next_j >= n:
                         next_i, next_j = i, j
                     
-                    value = -1 + Gamma * v[next_i, next_j]
+                    value = -1 + Gamma * v[next_i, next_j] # Bellman immediate cost (-1 + future value actualized)
                     max_value = max(max_value, value)
+
+                    if value > best_max_value:
+                        print(value, best_max_value)
+                        best_max_value = value
+                        best_action = action
                 
                 v[i, j] = max_value
+                pi[i, j] = best_action
                 delta = max(delta, abs(old_v - v[i, j]))
         
         if delta <= threshold:
             break
-    
-    pi = np.zeros((n, n), dtype=int)
-    
-    for i in range(n):
-        for j in range(n):
-            if (i == 0 and j == 0) or (i == n-1 and j == n-1):
-                continue
-            
-            max_value = float('-inf')
-            best_action = 0
-            
-            for action in range(4):
-                next_i = i + policy_one_step_look_ahead[action][0]
-                next_j = j + policy_one_step_look_ahead[action][1]
-                
-                if next_i < 0 or next_i >= n or next_j < 0 or next_j >= n:
-                    next_i, next_j = i, j
-                
-                value = -1 + Gamma * v[next_i, next_j]
-                
-                if value > max_value:
-                    max_value = value
-                    best_action = action
-            
-            pi[i, j] = best_action
     
     return pi, v
 
